23dd9851222129d9f7f839572d94a3bf7e348bfc
Rümeysa yük <19110131041@ogr.ksu.edu.tr>
2021-09-10 21:03:32 +0300

    .

--- main.py
+++ main.py
@@ -18,11 +18,37 @@ print(model)
 
 # print(model.wv["ankara"])
 
-print(model.wv.most_similar("hollanda"))
+# print(model.wv.most_similar("hollanda"))
 # modele kelimeleri verip eğittik.Ülkeleri bağdaştırıp bize getirdi.
 
-print(model.wv.most_similar("pazartesi"))
+# print(model.wv.most_similar("pazartesi"))
 # gün ile ilgili bağdaştırdıklarını getiriyor.
 
 model.save("word2vec.model")
 model = Word2Vec.load("word2vec.model")
+
+
+def closeswords_tsneplot(model, word):
+    word_vectors = np.empty((0, 100))
+    word_labels = [word]
+
+    close_words = model.wv.most_similar(word)
+    word_vectors = np.append(word_vectors, np.array([model.wv[word]]), axis=0)
+    # axis=0 verilmezse vektörler düzleştirilir.
+
+    for w, _ in close_words:
+        word_labels.append(w)
+        word_vectors = np.append(word_vectors, np.array([model.wv[w]]), axis=0)
+    tsne = TSNE(random_state=0)
+    y = tsne.fit_transform(word_vectors)
+
+    x_coords = y[:, 0]
+    y_coords = y[:, 1]
+
+    plt.scatter(x_coords, y_coords)
+    for label, x, y in zip(word_labels, x_coords, y_coords):
+        plt.annotate(label, xy=(x, y), xytext=(5, -2), textcoords="offset points")
+    plt.show()
+
+
+closeswords_tsneplot(model, "salı")
